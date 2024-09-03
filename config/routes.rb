e7b8215795b8aa99006c180fb4afdfb8746be895
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-09-04 08:18:20 +1000

    changed routes to /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  # APIroutes should be in /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
